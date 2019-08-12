057fda0facab479486222daf56d37ad67b962f37
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-12 13:21:12 -0700

    create cart context

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -2,8 +2,5 @@ import { createContext } from 'react';
 import { ProductData } from '../data';
 
 export default createContext({
-  products: [] as ProductData[],
-  addItem: (item: ProductData): void => {
-    console.log(item);
-  },
+  cart: [] as ProductData[],
 });
